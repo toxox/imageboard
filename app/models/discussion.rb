0ca9043eee0f17e92c255ca5c4049c70f50ed856
toxox <toxa1993@gmail.com>
2013-05-08 22:00:53 +0300

    Add associations

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,2 +1,8 @@
 class Discussion < ActiveRecord::Base
+  has_many :posts, dependent: :destroy
+
+  validates :comment, :subject, presence: true
+  validates :comment, length: { maximum: 1000 }
+  validates :subject, length: { maximum: 25 }
+  validates :name, length: { maximum: 15 }
 end

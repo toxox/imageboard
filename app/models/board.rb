33b2a35e89229ba1de0c549b5cc5c8f3b257efbb
toxox <toxa1993@gmail.com>
2013-07-02 15:06:48 +0300

    Add validations to board.

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,6 @@
 class Board < ActiveRecord::Base
   has_many :discussions, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :short_name, presence: true, length: { maximum: 5 }
 end

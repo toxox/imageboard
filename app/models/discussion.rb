68f89f310e97e6e0d3e5af90ef287f722082aafc
toxox <toxa1993@gmail.com>
2013-07-18 16:37:51 +0300

    Add number of discussions limit for boards.

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,6 +1,6 @@
 class Discussion < ActiveRecord::Base
   include EmptyNameHelper
-  before_save :set_name_to_anon_if_empty
+  before_save :set_name_to_anon_if_empty, :destroy_old_discussions
   before_create :set_last_post_at
 
   has_many :posts, dependent: :destroy
@@ -15,5 +15,11 @@ class Discussion < ActiveRecord::Base
 
   def set_last_post_at
     self.last_post_at = self.created_at || Time.now
+  end
+
+  def destroy_old_discussions
+    if Discussion.count >= 30
+      Discussion.order('last_post_at').first.destroy
+    end
   end
 end

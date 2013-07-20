96ca76d3e975b6e4be20d6af36ba3f01b100bb81
toxox <toxa1993@gmail.com>
2013-07-20 16:16:38 +0300

    Remove make_links_helper.rb.

--- app/helpers/make_links_helper.rb
+++
@@ -1,5 +0,0 @@
-module MakeLinksHelper
-  def make_links(string)
-    string.gsub(/((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?)/, '<a href=\'\1\'>\1</a>')
-  end
-end

a920676d2fd82e7d0ac3aa9ab7e40dc8b475dbf6
toxox <toxa1993@gmail.com>
2013-07-20 16:13:36 +0300

    Move comment-related helper methods to one module.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,12 +1,14 @@
-module FilterCommentHelper
-  def filter_comment(comment)
+module CommentsHelper
+  def make_links(comment)
+    comment.gsub(/((http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?)/, '<a href=\'\1\'>\1</a>')
+  end
 
+  def filter_comment(comment)
     str1 = '<span class = "quote">' #open greentext
     str2 = '</span>' #close greentext
 
     if comment != nil
       arr_lines = comment.split(/\n/)
-
       arr_lines.each do |a|
         if a[0] == ">"
           a.insert(0, str1)

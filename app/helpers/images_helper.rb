0529ea5783a13e31dec4e215fa8be4ec89f38610
toxox <toxa1993@gmail.com>
2013-07-21 13:57:23 +0300

    Images now open in a new tab when clicked.

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,6 +1,8 @@
 module ImagesHelper
   def thumb_with_link(model)
+    if model.image?
     link_to image_tag(model.image_url(:thumb)),
-                           model.image_url if model.image?
+                           model.image_url, target: "_blank"
+    end
   end
 end

d3a13590809c9fcda87540577fbaa1b71611a79d
toxox <toxa1993@gmail.com>
2013-08-26 11:26:42 +0300

    Remove existing tests to start from clean slate.

--- spec/models/board_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Board do
-  let(:board) { FactoryGirl.create :board }
-
-  it { should respond_to(:name) }
-  it { should respond_to(:short_name) }
-  it { should respond_to(:discussions) }
-end
